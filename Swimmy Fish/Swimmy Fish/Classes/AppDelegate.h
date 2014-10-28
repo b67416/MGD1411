0100eace0b28dd4942f6572967cd654a0bfc9600
Ryan Wahle <ryanwahle@fullsail.edu>
2014-10-28 16:16:00 -0700

    initial framework. doesn't even compile right now

---
+++ Swimmy Fish/Swimmy Fish/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Swimmy Fish
+//
+//  Created by Ryan Wahle on 10/28/14.
+//  Copyright Ryan Wahle 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
